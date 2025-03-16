dfff1a41bc4665aea3914522e2a4e15263576db1
Ashal <shaulca123@gmail.com>
2025-03-16 14:49:29 +0200

    cd-test

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ import boto3
 # Don't specify credentials in order to use the default ones that exist on the ECS task
 s3_client = boto3.client('s3')
 # Get the object from the bucket
-s3_object = s3_client.get_object(Bucket='ashal-lab7-s3', Key='logo.png')
+s3_object = s3_client.get_object(Bucket='ashal-lab7-s3', Key='logo192.png')
 logo = s3_object['Body'].read()
 # Save it to disk in order to serve it as a static resource
 with open('logo', 'wb') as f:
